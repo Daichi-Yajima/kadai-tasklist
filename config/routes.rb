b861b1999c9a90d3b8deac35cbf9abfb893be98e
daichi.yajima <d.0331.y@gmail.com>
2018-08-04 20:52:21 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
   root to: 'toppages#index'
-
-  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -10,4 +7,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks, only: [:create, :destroy]
 end
