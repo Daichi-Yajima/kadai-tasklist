cf10c92ce0d29dae1d156eb15160469b6eb436a9
daichi.yajima <d.0331.y@gmail.com>
2018-08-09 02:37:04 +0000

    sai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   
   def index
     @tasks = Task.all
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end
   
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @task = current_user.tasks.find_by(id: params[:id])
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -21,7 +20,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
